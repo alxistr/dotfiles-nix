f02118a6aa12335f56629fff496f90551340ba6c
Alex <alxistr@gmail.com>
2019-09-08 07:48:23 +0300

    .

--- dotfiles/shell/default.nix
+++ dotfiles/shell/default.nix
@@ -15,6 +15,7 @@
     tree
     less
     unzip
+    unrar
     p7zip
     atool
     rlwrap
--- overlay/packages/adhosts/default.nix
+++ overlay/packages/adhosts/default.nix
@@ -1,10 +1,20 @@
 { pkgs, stdenv, lib }:
+
+let whitelist = pkgs.writeTextFile {
+  name = "adhosts-whitelist";
+  text = ''
+    binance.com
+  ''; 
+}; in
+
 stdenv.mkDerivation rec {
   name = "adhosts";
   src = builtins.fetchurl "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts";
   unpackPhase = ":";
   buildPhase = ''
-    awk '/^0.0.0.0 / { printf "address=/%s/0.0.0.0\n", $2 }' $src > $out 
+    grep -Fv -f ${whitelist} $src | \
+    awk '/^0.0.0.0 / { printf "address=/%s/0.0.0.0\n", $2 }' > \
+    $out 
   '';
   installPhase = ":";
 }
