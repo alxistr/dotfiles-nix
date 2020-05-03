858a1c4da9e92d05e19c5b3bb5e9ec0cd002faca
Alex <alxistr@gmail.com>
2020-05-03 11:03:04 +0300

    .

--- overlay/packages/scdl/default.nix
+++ overlay/packages/scdl/default.nix
@@ -7,9 +7,11 @@ buildPythonApplication rec {
 
   patches = [ ./home.patch ];
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "0fij1kqbcm5i6c4g7lbqrmrzw7xvhrawg71zgsvbayn7d6x5s4yg";
+  src = fetchGit {
+    url = "https://github.com/flyingrub/scdl.git";
+    rev = "b0a164f843d7d7282f4f2e742892e0239d282a91";
+    # inherit pname version;
+    # sha256 = "0fij1kqbcm5i6c4g7lbqrmrzw7xvhrawg71zgsvbayn7d6x5s4yg";
   };
 
   propagatedBuildInputs = [
