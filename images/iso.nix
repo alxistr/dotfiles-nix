e7c206425ce28e8c1656b84e48f1b58389281670
Alex <alxistr@gmail.com>
2020-10-25 07:59:26 +0300

    .

--- images/iso.nix
+++ images/iso.nix
@@ -1,4 +1,10 @@
 { config, pkgs, lib, ... }:
+
+let
+  # squashfs-compression = "xz -Xdict-size 100%";
+  squashfs-compression = "gzip -noD -noF -noX -noI";
+in
+
 {
   imports = [
     <nixpkgs/nixos/modules/installer/cd-dvd/iso-image.nix>
@@ -21,20 +27,19 @@
     virtualisation.enable = true;
   };
 
+  system.build.squashfsStore = pkgs.callPackage <nixpkgs/nixos/lib/make-squashfs.nix> {
+    storeContents = config.isoImage.storeContents;
+    comp = squashfs-compression;
+  };
+
   isoImage = {
     isoName = "${config.isoImage.isoBaseName}-${config.system.nixos.label}-${pkgs.stdenv.hostPlatform.system}.iso";
 
     makeEfiBootable = true;
     makeUsbBootable = true;
 
-    efiSplashImage = pkgs.fetchurl {
-      url = https://raw.githubusercontent.com/NixOS/nixos-artwork/a9e05d7deb38a8e005a2b52575a3f59a63a4dba0/bootloader/efi-background.png;
-      sha256 = "18lfwmp8yq923322nlb9gxrh5qikj1wsk6g5qvdh31c4h5b1538x";
-    };
-    splashImage = pkgs.fetchurl {
-      url = https://raw.githubusercontent.com/NixOS/nixos-artwork/a9e05d7deb38a8e005a2b52575a3f59a63a4dba0/bootloader/isolinux/bios-boot.png;
-      sha256 = "1wp822zrhbg4fgfbwkr7cbkr4labx477209agzc0hr6k62fr6rxd";
-    };
+    efiSplashImage = "${pkgs.wallpapers}/skull.jpg";
+    splashImage = "${pkgs.wallpapers}/skull.jpg";
 
     appendToMenuLabel = "";
     grubTheme = null;
