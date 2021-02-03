9900926956b8d16c861c83b770f573e506da8001
Alex <alxistr@gmail.com>
2021-02-03 03:10:58 +0300

    .

--- modules/nixos/network.nix
+++ modules/nixos/network.nix
@@ -1,6 +1,7 @@
 { config, pkgs, lib, ... }:
 
 let cfg = config.own; in
+let wg = cfg.wireguard; in
 with lib; with types;
 
 {
@@ -25,44 +26,56 @@ with lib; with types;
     };
   };
 
-  config = {
-    time.timeZone = mkDefault "Europe/Moscow";
+  config = mkMerge [
+    {
+      time.timeZone = mkDefault "Europe/Moscow";
 
-    networking.nameservers = [ "8.8.8.8" "8.8.4.4" ];
+      networking.nameservers = [ "8.8.8.8" "8.8.4.4" ];
 
-    networking.wireless = mkIf (cfg.wifi != { }) {
-      enable = true;
-      networks = cfg.wifi;
-    };
-
-    services.openvpn.servers = cfg.ovpn;
-
-    networking.wireguard.interfaces = with cfg.wireguard; mkIf (enable) {
-      wg0 = {
-        ips = [ "${ip}/32" ];
-        inherit privateKey;
-        allowedIPsAsRoutes = false;
-        peers = [
-          {
-            publicKey = server.publicKey;
-            allowedIPs = [ "0.0.0.0/0" ];
-            endpoint = "${server.host}:${toString server.port}";
-            persistentKeepalive = 25;
-          }
-        ];
-        postSetup = ''
-          default_gateway=$(ip route | ${pkgs.gawk}/bin/awk '/default/ { print $3 }')
-          ip ro add 0.0.0.0/1 via ${ip} dev wg0
-          ip ro add 128.0.0.0/1 via ${ip} dev wg0
-          ip ro add ${server.host} via $default_gateway
-          ip ro flush cache
-        '';
-        postShutdown = ''
-          ip ro del ${server.host}
-        '';
+      networking.wireless = mkIf (cfg.wifi != { }) {
+        enable = true;
+        networks = cfg.wifi;
       };
-    };
 
-  };
+      services.openvpn.servers = cfg.ovpn;
+
+    }
+
+    (mkIf wg.enable {
+      networking.firewall.checkReversePath = false;
+
+      networking.localCommands = ''
+          ${pkgs.iproute}/bin/ip ro add default via 127.0.0.1 dev lo table 2468 metric 200
+      '';
+
+      networking.wireguard.interfaces = {
+        wg0 = {
+          ips = [ "${wg.ip}/32" ];
+          privateKey = wg.privateKey;
+          allowedIPsAsRoutes = false;
+          peers = [
+            {
+              publicKey = wg.server.publicKey;
+              allowedIPs = [ "0.0.0.0/0" ];
+              endpoint = "${wg.server.host}:${toString wg.server.port}";
+              persistentKeepalive = 25;
+            }
+          ];
+          postSetup = ''
+            wg set wg0 fwmark 1234
+            ip route add default dev wg0 table 2468 metric 100
+            ip rule add not fwmark 1234 table 2468
+            ip rule add table main suppress_prefixlength 0
+          '';
+          postShutdown = ''
+            ip route del default dev wg0 table 2468
+            ip rule del not fwmark 1234 table 2468
+            ip rule del table main suppress_prefixlength 0
+          '';
+        };
+      };
+
+    })
+  ];
 
 }
