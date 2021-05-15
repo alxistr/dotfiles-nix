15f451f5814778ebca46a9a5162b9270e46f5bc3
Alex <alxistr@gmail.com>
2021-05-15 15:51:12 +0300

    .

--- hosts/desktop/hardware-configuration.nix
+++ hosts/desktop/hardware-configuration.nix
@@ -25,6 +25,7 @@
       "nixos-encrypted".device = "/dev/disk/by-uuid/508f5923-d0a4-48c7-bd82-1f2b730d9188";
       "dataf".device = "/dev/disk/by-uuid/5d0d55c1-2c70-41dd-abcb-cd15640d9547";
       "datal".device = "/dev/disk/by-uuid/a0dc4c66-e374-43e3-8279-ba4622f63455";
+      "data2".device = "/dev/disk/by-uuid/0ac15c98-05d4-4acd-9a8e-67cf456f0b2d";
     };
   };
 
@@ -41,12 +42,12 @@
       device = "/dev/disk/by-uuid/a4a9d2fe-bf1a-435b-8e6a-f152735d863c";
       fsType = "btrfs";
     };
-    "/mnt/datas/" = {
-      device = "/dev/disk/by-uuid/a20a34d0-35da-469b-a745-2e45d4040cc3";
-      fsType = "ext4";
-    };
     "/mnt/datal/" = {
       device = "/dev/disk/by-uuid/0260de44-87ec-4ec0-a5fc-328f9e00d782";
+      fsType = "btrfs";
+    };
+    "/mnt/datat/" = {
+      device = "/dev/disk/by-uuid/089afb8f-3020-4262-84b5-3c2b0c365b71";
       fsType = "btrfs";
     };
   };
