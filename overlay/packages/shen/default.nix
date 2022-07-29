74cd7c9f6566056a4257b1b433fbcd7251870967
Alex <alxistr@gmail.com>
2022-07-29 04:28:14 +0300

    .

--- overlay/packages/shen/default.nix
+++ overlay/packages/shen/default.nix
@@ -1,20 +1,22 @@
-{ pkgs, stdenv, lib, git, makeWrapper, clpkg ? pkgs.ecl }:
+{ pkgs, stdenv, writeScriptBin, rlwrap, clpkg ? pkgs.ecl }:
 
-let clname = clpkg.pname; in
+let
+  clname = clpkg.pname;
+  shen = stdenv.mkDerivation rec {
+    name = "shen-${clname}";
+    version = "3.0.3";
+    src = builtins.fetchurl "https://github.com/Shen-Language/shen-cl/releases/download/v${version}/shen-cl-v${version}-sources.tar.gz";
+    buildInputs = [ clpkg ];
+    buildPhase = ''
+      make build-${clname}
+      mkdir -p $out/bin
+      cp -r bin/${clname}/shen $out/bin/shen-${clname}
+    '';
+    installPhase = ":";
+  };
+in
 
-stdenv.mkDerivation rec {
-  name = "shen-${clname}";
-  version = "3.0.3";
-  src = builtins.fetchurl "https://github.com/Shen-Language/shen-cl/releases/download/v${version}/shen-cl-v${version}-sources.tar.gz";
-
-  buildInputs = [ makeWrapper clpkg ];
-
-  buildPhase = ''
-    make build-${clname}
-    mkdir -p $out/bin
-    cp -r bin/${clname}/shen $out/bin/shen
-  '';
-
-  installPhase = ":";
-
-}
+writeScriptBin "shen"
+ ''
+   ${rlwrap}/bin/rlwrap ${shen}/bin/shen-${clname} $@
+ ''
