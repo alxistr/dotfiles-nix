39e1bdfd20a84e333e3671a62eeaa1a6db67f1f4
Alex <alxistr@gmail.com>
2022-10-22 10:06:53 +0300

    .

---
+++ scripts/ssd-up-1.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+(
+  read -sr -p "pass: " passwd
+
+  echo $passwd | sudo cryptsetup luksOpen /dev/sdb2 container-sdb2 -
+  echo $passwd | sudo cryptsetup luksOpen /dev/sdb3 container-sdb3 -
+
+  mkdir -p /tmp/luks/{sdb2,sdb3}
+
+  (
+    cd /tmp/luks
+    sudo mount /dev/mapper/container-sdb2 ./sdb2
+    sudo mount /dev/mapper/container-sdb3 ./sdb3
+
+    # echo -e "\033[0;31msession with mounted ssd\033[0m" && bash
+    ranger
+
+    sudo umount ./sdb2 ./sdb3
+
+  )
+
+  sudo cryptsetup luksClose container-sdb2
+  sudo cryptsetup luksClose container-sdb3
+
+)
